a3ba4e6ca2a559fd489079c40bbd84c914a58c32
空樱酱 <ritsuka.sunny@gmail.com>
2015-11-15 19:06:51 +0800

    first commit

---
+++ Plugin.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Parsedown
+ *
+ * @package Parsedown Plugin
+ * @author Kokororin
+ * @version 1.0
+ * @link https://kotori.love
+ */
+
+class Parsedown_Plugin implements Typecho_Plugin_Interface
+{
+    /**
+     * 激活插件
+     */
+    public static function activate()
+    {
+        Typecho_Plugin::factory('Widget_Abstract_Contents')->markdown = array('Parsedown_Plugin', 'markdown');
+        Typecho_Plugin::factory('Widget_Abstract_Comments')->markdown = array('Parsedown_Plugin', 'markdown');
+    }
+
+    /**
+     * 禁用插件
+     */
+    public static function deactivate()
+    {}
+
+    /**
+     * 插件设置
+     */
+    public static function config(Typecho_Widget_Helper_Form $form)
+    {}
+
+    public static function personalConfig(Typecho_Widget_Helper_Form $form)
+    {}
+
+    public static function markdown($text)
+    {
+        require_once dirname(__FILE__) . '/Parsedown.php';
+        return Parsedown::instance()->text($text);
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+### Typecho Parsedown 解析markdown插件
+
+使用[Parsedown](https://github.com/erusev/parsedown)替换Typecho自带的[Markdown解析库](http://michelf.com/projects/php-markdown)。
